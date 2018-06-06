36e7a722def239cfeb47f11f1063c773835ddb35
krishnapokhare@gmail.com <tigers@1234>
2018-06-05 23:20:00 -0700

    Added Search functionality

---
+++ app/src/main/java/com/pokhare/mymoviedb/adapters/SearchAdapter.java
@@ -0,0 +1,47 @@
+package com.pokhare.mymoviedb.adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class SearchAdapter extends ArrayAdapter {
+    private List<String> dataList;
+    private Context mContext;
+    private int searchResultItemLayout;
+
+    public SearchAdapter(Context context, int resource, List<String> storeSourceDataLst) {
+        super(context, resource, storeSourceDataLst);
+        dataList = storeSourceDataLst;
+        mContext = context;
+        searchResultItemLayout = resource;
+    }
+
+    @Override
+    public int getCount() {
+        return dataList.size();
+    }
+
+    @Override
+    public String getItem(int position) {
+        return dataList.get(position);
+    }
+
+    @Override
+    public View getView(int position, View view, @NonNull ViewGroup parent) {
+
+        if (view == null) {
+            view = LayoutInflater.from(parent.getContext())
+                    .inflate(searchResultItemLayout, parent, false);
+        }
+
+        TextView resultItem = (TextView) view.findViewById(android.R.id.text1);
+        resultItem.setText(getItem(position));
+        return view;
+    }
+}
